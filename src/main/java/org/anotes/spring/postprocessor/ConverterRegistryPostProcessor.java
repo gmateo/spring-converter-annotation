a0a9856de6bbb47f62fd668a046d4be5c9da7bbe
Gamal <gamal.mateo@gmail.com>
2013-03-21 19:11:50 -0500

    implementing beanpostprocessor

--- src/main/java/org/anotes/spring/postprocessor/ConverterRegistryPostProcessor.java
+++ src/main/java/org/anotes/spring/postprocessor/ConverterRegistryPostProcessor.java
@@ -1,34 +1,52 @@
 package org.anotes.spring.postprocessor;
 
-import org.anotes.spring.stereotype.TypeConverter;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.support.ConversionServiceFactoryBean;
 import org.springframework.core.convert.converter.Converter;
-import org.springframework.core.convert.support.DefaultConversionService;
 
-import java.util.Collection;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * User: gmc
  * Date: 14/03/13
  */
-public class ConverterRegistryPostProcessor implements BeanDefinitionRegistryPostProcessor {
+public class ConverterRegistryPostProcessor implements BeanDefinitionRegistryPostProcessor, BeanPostProcessor, ApplicationContextAware {
+    private final String CONVERSION_SERVICE_NAME = "conversionService";
 
     public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException {
-        registry.registerBeanDefinition("conversionService", BeanDefinitionBuilder.rootBeanDefinition(ConversionServiceFactoryBean.class).getBeanDefinition());
+        registry.registerBeanDefinition(CONVERSION_SERVICE_NAME, BeanDefinitionBuilder.rootBeanDefinition(ConversionServiceFactoryBean.class).getBeanDefinition());
     }
 
     public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
-        Map<String, Object> beansWithAnnotation = beanFactory.getBeansWithAnnotation(TypeConverter.class);
-        Collection converters = beansWithAnnotation.values();
-        DefaultConversionService conversionService = (DefaultConversionService) beanFactory.getBean("conversionService");
-        for (Object converter : converters) {
-            conversionService.addConverter((Converter<?, ?>) converter);
-        }
     }
+
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        if (CONVERSION_SERVICE_NAME.equals(beanName)) {
+            Map<String, Converter> beansOfType = appCtx.getBeansOfType(Converter.class);
+            ConversionServiceFactoryBean conversionfactoryBean = (ConversionServiceFactoryBean) bean;
+            Set converters = new HashSet(beansOfType.values());
+            conversionfactoryBean.setConverters(converters);
+        }
+        return bean;
+    }
+
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        return bean;
+    }
+
+    ApplicationContext appCtx;
+
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        appCtx = applicationContext;
+    }
+
 }
